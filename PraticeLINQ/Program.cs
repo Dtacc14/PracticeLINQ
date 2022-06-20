fc33a3c8bbfa094edfb834ec902f9efc2ca8233c
David Taccetta <taccetta14@gmail.com>
2022-06-20 15:25:12 -0400

    Initial PracticeLINQ Commit

---
+++ PraticeLINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PraticeLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGames = new List<string>() { "NCAA", "Madden", "Call of Duty", "Rocket League", "Fortnite", "Legend of Zelda", "Left 4 Dead" };
+
+            Console.WriteLine("Here is a list of the games I play:");
+            videoGames.OrderBy(x => x.Length).ToList().ForEach(Console.WriteLine);      
+        }
+    }
+}
